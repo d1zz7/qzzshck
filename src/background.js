7dc8da29ed3bd1c674f728a6693b0f562947f9c4
d1zz7 <muhametov.tahir@yandex.ru>
2022-12-14 00:21:24 +0600

    init

--- src/background.js
+++ src/background.js
@@ -1,21 +1,12 @@
 'use strict';
-
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
+const messageTypes = require("./messageTypes");
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
+  switch (request.type) {
+    case messageTypes.MOUSE_CLICK:
+      sendResponse("YEP!");
+      break;
+    default:
+      break;
   }
 });
---
+++ src/service.js
@@ -0,0 +1,15 @@
+const sendMessage = (type = null, payload = null) => {
+  chrome.runtime.sendMessage(
+    {
+      type,
+      payload,
+    },
+    (response) => {
+       console.log(response);
+    }
+  );
+};
+
+module.exports = {
+  sendMessage,
+};
